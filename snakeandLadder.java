255eb48df1c28fc350868799e1fa8d128c5e7a41
shubhampatte <shubhampatte@gmail.com>
2021-10-02 02:02:31 +0530

    [shubham] : Position of player

--- snakeandLadder.java
+++ snakeandLadder.java
@@ -6,7 +6,12 @@ public class snakeandLadder
 		int startPosition = 0;
 		System.out.println("Starting position of player : " +startPosition);
 			
+		int diceRollValue = (int) (Math.random()*6)+1;
+		System.out.println("Dice value :" +diceRollValue);
 		
+		startPosition += diceRollValue;
+		System.out.println("Current position after dice roll :" +startPosition);
+
 		
 	}
 }
